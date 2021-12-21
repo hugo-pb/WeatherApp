96d065835fed05c8bef615cf60732c4859221b6a
Hugo Palomera <85078686+hugo-pb@users.noreply.github.com>
2021-12-21 11:26:15 -0700

    added weekdays

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="row">
-        <div className="col-md-2"> </div>
-        <div className="col-md-5">
+        <div className="col-sm-2"> </div>
+        <div className="col-sm-5">
           <LeftCard />
         </div>
-        <div className="col-md-3">
+        <div className="col-sm-3">
           <RightCard />
         </div>
-        <div className="col-md-2"> </div>
+        <div className="col-sm-2"> </div>
       </div>
     </div>
   );
--- src/RightCard.js
+++ src/RightCard.js
@@ -4,9 +4,28 @@ export default function RightCard() {
   return (
     <div className="RightCard">
       <div className="card">
-        <p>Percipitation</p>
-        <p>Humidity</p>
-        <p>wind</p>
+        <ul>
+          <li>Percipitation 0%</li>
+          <li>Humidity 15%</li>
+          <li>wind 13km/h</li>
+        </ul>
+        <div className="week">
+          <ul>
+            <li> </li>
+            <li>Tue</li>
+            <li>29C </li>
+          </ul>
+          <ul>
+            <li> </li>
+            <li>Wed </li>
+            <li>20C </li>
+          </ul>
+          <ul>
+            <li> </li>
+            <li>Thu </li>
+            <li>25C</li>
+          </ul>
+        </div>{" "}
       </div>
     </div>
   );
